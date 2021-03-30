7a50f3f9f6e3aa13a18d57dd9b468540677e696c
Lilian <lilianouvmmi@gmail.com>
2021-03-30 21:18:55 +0200

    set light color palette

--- js/theme.js
+++ js/theme.js
@@ -6,15 +6,21 @@ const changeTheme = () => {
 const applyTheme = () => {
     if (localStorage.getItem('theme') === 'light') {
         localStorage.setItem('theme', 'light');
-        document.documentElement.style.setProperty('--main-color', '#ffecd4');
-        document.documentElement.style.setProperty('--seconday-color', '#4bafff');
+        document.documentElement.style.setProperty('--main-color', '#f1f1f1');
+        document.documentElement.style.setProperty('--seconday-color', '#65a1fb');
         document.documentElement.style.setProperty('--white-text-color', '#3a3636');
+        document.documentElement.style.setProperty('--bg-text-menu', '#dcdcdcb8');
+        document.documentElement.style.setProperty('--banner-color', '#dedede');
+        document.documentElement.style.setProperty('--banner-shadow-color', '#dadadab8');
         document.querySelector('.logo').setAttribute('src', '/images/logo-black.svg');
     } else {
         localStorage.setItem('theme', 'dark');
         document.documentElement.style.setProperty('--main-color', '#171717');
         document.documentElement.style.setProperty('--seconday-color', '#5ae1b4');
         document.documentElement.style.setProperty('--white-text-color', '#dcdcdc');
+        document.documentElement.style.setProperty('--bg-text-menu', '#242424b8');
+        document.documentElement.style.setProperty('--banner-color', '#111');
+        document.documentElement.style.setProperty('--banner-shadow-color', '#0000006e');
         document.querySelector('.logo').setAttribute('src', '/images/logo.svg');
     }
 }
